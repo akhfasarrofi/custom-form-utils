87eb22d31ce78611034ecd4d9b8891dd4d54a045
akhfasarrofi <zakaria_akhfas@outlook.co.id>
2024-02-03 15:14:31 +0700

    chore: update config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,27 +2,30 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    "plugin:prettier/recommended",
+    'plugin:prettier/recommended',
+    'plugin:react/jsx-runtime',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier', 'react-hooks'],
   rules: {
-    "prettier/prettier": [
-      "off",
-      {
-        "singleQuote": true,
-        "parser": "flow"
-      }
-    ],
     "no-console": "warn",
     "no-unused-vars": ["error", { args: "none" }],
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+    "react/jsx-props-no-spreading": "off",
+    "react/require-default-props": "off",
+    "react/function-component-definition": ["error", { namedComponents: "arrow-function" }],
+    "import/prefer-default-export": "off",
+    "no-empty": "warn",
   },
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
   build: {
     lib: {
-      entry: "src/index.ts",
-      name: "custom-form-utils",
-      formats: ["es"],
+      entry: 'src/index.ts',
+      name: 'custom-form-utils',
+      formats: ['es'],
     },
     rollupOptions: {
-      external: ["react", "antd", "react-dom"], // Add your external dependencies here
-      output: {
-        sourcemap: true,
-      },
+      external: ['react', 'antd', 'react-dom'], // Add your external dependencies here
       plugins: [dts()],
     },
   },
