e01e1847016830c821cc59903e8eaf7bfe91fe74
dopassss <zakaria.akhfas@gmail.com>
2024-06-16 16:37:36 +0700

    chore: Update TypeScript configuration and build script

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,35 @@
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
+import { glob } from 'glob';
+import { extname, relative, resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), libInjectCss(), dts({ include: ['lib'] })],
   build: {
-    rollupOptions: {
-      external: ['react', 'react-dom'], // Add your external dependencies here
-      plugins: [dts()],
+    copyPublicDir: false,
+    lib: {
+      entry: new URL('lib/main.ts', import.meta.url).pathname,
+      formats: ['es'],
     },
-    sourcemap: true,
-  },
-  optimizeDeps: {
-    exclude: ['react', 'react-dom'], // Add your external dependencies here
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime'],
+      input: glob.sync('lib/**/*.ts').reduce(
+        (acc, file) => {
+          const name = relative(resolve('lib'), file).replace(
+            extname(file),
+            ''
+          );
+          acc[name] = file;
+          return acc;
+        },
+        {} as Record<string, string>
+      ),
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
+      },
+    },
   },
 });
