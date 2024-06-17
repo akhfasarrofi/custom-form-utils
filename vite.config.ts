6420eab16221bbda38b63bf28381c73e6ec65046
dopassss <zakaria.akhfas@gmail.com>
2024-06-17 20:38:09 +0700

    chore: Update package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
+import { fileURLToPath } from 'node:url';
+
 import react from '@vitejs/plugin-react';
 import { glob } from 'glob';
-import { extname, relative, resolve } from 'path';
+import { extname, relative } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import { libInjectCss } from 'vite-plugin-lib-inject-css';
@@ -15,16 +17,20 @@ export default defineConfig({
     },
     rollupOptions: {
       external: ['react', 'react/jsx-runtime'],
-      input: glob.sync('lib/**/*.ts').reduce(
-        (acc, file) => {
-          const name = relative(resolve('lib'), file).replace(
-            extname(file),
-            ''
-          );
-          acc[name] = file;
-          return acc;
-        },
-        {} as Record<string, string>
+      input: Object.fromEntries(
+        // https://rollupjs.org/configuration-options/#input
+        glob
+          .sync('lib/**/*.{ts,tsx}', {
+            ignore: ['lib/**/*.d.ts'],
+          })
+          .map(file => [
+            // 1. The name of the entry point
+            // lib/nested/foo.js becomes nested/foo
+            relative('lib', file.slice(0, file.length - extname(file).length)),
+            // 2. The absolute path to the entry file
+            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
       ),
       output: {
         assetFileNames: 'assets/[name][extname]',
